0cf8a556065e58b752837d719a32605da883fdb7
Britta-Schulte <70574852+Britta-Schulte@users.noreply.github.com>
2022-05-19 15:54:49 +0200

    Update WP1_Zeitung_D_U18.js

--- data_includes/WP1_Zeitung_D_U18.js
+++ data_includes/WP1_Zeitung_D_U18.js
@@ -247,7 +247,7 @@ newTrial("Meta1",
                ,
                //Alter
                newDropDown("age", "Bitte eine Option ausw&auml;hlen")
-               .settings.add("18" , "19" , "20", "21" , "22" , "23", "24" , "25" , "26", "27" , "28" , "29", "30" , "31", "32","33", "34" , "35", "36","37","38","39","über 40")
+               .settings.add("12" , "13" , "14", "15" , "16 , "17", "18" ,"anderes")
                .log()
 
                ,
